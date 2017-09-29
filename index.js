2140abf3f3762c86baaf92e1ea5601e17cd1886f
tahseen447 <tahseenfatima1986@gmail.com>
2017-09-29 17:02:16 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(a, b)
+{
+  return a+b;
+}
+
+function subtract(a,b)
+{
+  return a-b;
+}
+
+function multiplies (a,b)
+{
+  return a*b
+}
+function divide(a, b)
+{
+  return  a/b
+}
+function inc(n)
+{
+  return ++n;
+}
+function dec(n)
+{
+  return --n
+}
+function makeInt(n)
+{
+  return n.parseInt()
+}
